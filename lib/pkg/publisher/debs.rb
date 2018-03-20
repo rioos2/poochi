1c29a068d179f72e4eb09ff002216f18d30e21c6
indykish <nkishore@megam.io>
2018-03-20 18:32:07 +0000

    Fixes to make sure reprepro command is run.

--- lib/pkg/publisher/debs.rb
+++ lib/pkg/publisher/debs.rb
@@ -8,15 +8,15 @@ module Pkg
   class DebsPublisher < BasePublisher
     attr_accessor :template
 
-    NAME = "debs".freeze
-    REPREPRO = "reprepro".freeze
+    NAME = "deb"
+    REPREPRO = "reprepro"
 
     def initialize(ship_paths)
       super(ship_paths)
 
       distribute_erb     = NAME << ".distribution.erb"
 
-      erb_path = Pathname(File.expand_path(File.dirname(__FILE__))).to_s + '/../../#{distribute_erb}'
+      erb_path = Pathname(File.expand_path(File.dirname(__FILE__))).to_s << "/../../../" << distribute_erb
 
       @out_path  = @current_ship_path.ship_dist_file
 
@@ -29,7 +29,7 @@ module Pkg
 
     ## Write a reprepro distribution from the reprepro.distribution.erb
     def save
-      puts "=> Write #{out_path}".colorize(:green).bold
+      puts "=> 5. Write #{@out_path}".colorize(:green).bold
       FileUtils.rm(@out_path) if Pkg::Util::File.exists?(@out_path)
 
       File.open(@out_path, 'w+') do |f|
@@ -45,7 +45,7 @@ module Pkg
 
     def generate
       do_generate(deb_files)
-      puts "=> ✔  Reprepro: *.deb".colorize(:green).bold
+      puts "   ✔  reprepro: *.deb".colorize(:blue).bold
     end
 
     def publish
@@ -60,34 +60,37 @@ module Pkg
     end
 
     def ship_home_suffix(suffix)
-      @current_ship_path.ship_home << '/' << suffix
+      @current_ship_path.ship_home +  '/' +  suffix
     end
 
     def deb_files
       Pkg::Util::File::files_with_ext(@current_ship_path.ship_home, 'deb')
     end
 
-    def reprepo(deb_file )
-      REPREPRO <<  ' -Vb ' << ship_home  <<  ' includedeb ' << @current_ship_path.distro << deb_file
+    def reprepro(deb_file )
+      tmp = "  " <<  ' -Vb ' << Pkg::Config.packaging_repo  <<  ' includedeb ' << @current_ship_path.os << " "  << deb_file
+      REPREPRO + tmp
     end
 
     def do_generate(deb_files)
       deb_files.each do |deb_file|
-        Pkg::Util::Execution.ex(reprepro(debfile))
+        Pkg::Util::Execution.ex(reprepro(deb_file))
       end
     end
 
     def do_publish
       %w[db dists pool].each do |suffix|
-        ship_home_suffixed = ship_home(suffix)
-        Pkg::Util::File::cp_r(ship_home_suffixed, Pkg::Config.deb_html_root)
-        Pkg::Util::File::rm_rf(ship_home_suffixed)
+        ship_home_suffixed = ship_home_suffix(suffix)
+        unless !Pkg::Util::File::directory?(ship_home_suffixed)
+          Pkg::Util::File::cp_r(ship_home_suffixed, Pkg::Config.deb_html_root)
+          Pkg::Util::File::rm_rf(ship_home_suffixed)
+        end
       end
     end
 
     def set_permission
       ### What the hect is the build directory
-      Pkg::Util::Execution::ex("chmod -R ugo+rX $BUILD_dir")
+      #Pkg::Util::Execution::ex("chmod -R ugo+rX $BUILD_dir")
     end
 
   end
